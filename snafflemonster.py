78c6fd5262df51679e23898ddbd18bfbefa26066
Lincoln Short <lynkle@Lincolns-MBP.hololive>
2022-05-30 21:50:55 +0800

    Moved from sys.argv to argparse and added more arguments for future support

--- snafflemonster.py
+++ snafflemonster.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 import json
-import sys
+import argparse
+from xmlrpc.client import Boolean
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 # Disable insecure requests warning (it doesn't matter)
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning) 
 
-def load_data():
-    with open(sys.argv[1], "r") as snaffout:
+def load_data(filename:str):
+    with open(filename, "r") as snaffout:
         data = json.load(snaffout)
     return data
 
-def post_data_to_elastic(data):
+def post_data_to_elastic(data, index:str, apikey:str):
     triage_levels = ["Green", "Yellow", "Red", "Black"]
     snaffle = {}
     snaffle["entries"] = []
@@ -23,18 +24,33 @@ def post_data_to_elastic(data):
                 if(list(entry["eventProperties"].keys())[0] == triage_level):
                     if(entry["eventProperties"][triage_level]["Type"] == "FileResult"):
                         # send the data
-                        r = requests.put(f"https://10.48.100.119:9200/{sys.argv[2]}/_doc/{ii}", json=entry["eventProperties"][triage_level], headers={"Authorization": f"ApiKey {sys.argv[3]}"}, verify=False)
+                        r = requests.put(f"https://10.48.100.119:9200/{index}/_doc/{ii}", json=entry["eventProperties"][triage_level], headers={"Authorization": f"ApiKey {apikey}"}, verify=False)
                         if(r.status_code != 201):
                             print(r.status_code, r.reason)
                         # increment the counter 
                         ii = ii + 1
 
 def main():
-    if len(sys.argv) != 3:
-        print("3 arguments are required!\n1: The path to the JSON file to process\n2: The name of the index to store results in\n3: The API key to use for authentication.")
+    parser = argparse.ArgumentParser(description="Send Snaffler Output to ElasticSearch for analysis.", epilog="Happy Snaffling")
+    parser.add_argument("-f", "--file", type=str, help="The path to the JSON file to process.", required=True)
+    parser.add_argument("-i", "--index", type=str, help="The name of the index to store results in.")
+    parser.add_argument("-k", "--apikey", type=str, help="The API key used to authentiate to ElasticSearch.")
+    parser.add_argument("-r", "--replace", type=str, help="Optional argument to delete existing items in the index selected before adding new items.")
+    parser.add_argument("-a", "--append", type=str, help="Optional argument to append new items to the selected index.")
+    parser.add_argument("--insecure", type=Boolean, help="Toggle for allowing sending over HTTPS with verification turned off so self signed or invalid ceritficates can be used.")
+    args = parser.parse_args()
+
+    data = load_data(args.file)
+
+    if args.index is not None and args.apikey is not None:
+        post_data_to_elastic(data, args.index, args.apikey)
     else:
-        data = load_data()
-        post_data_to_elastic(data)
+        if args.index is None:
+            # TODO add functionality here using the ES library to check for existing indicies
+            index = input("Please enter a name for the index to use: ")
+        if args.apikey is None:
+            apikey = input("Please enter an API Key: ")
+        post_data_to_elastic(data, index, apikey)
 
 if __name__ == "__main__":
     main()
